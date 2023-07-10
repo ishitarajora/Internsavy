31b9bff60bfd7a8b2a1f53d2d71931479e422ccc
ishitarajora <ishita.rajora@gmail.com>
2023-07-10 15:28:25 +0530

    first commit

---
+++ Calculator/src/com/internsavy/calculator/Operation.java
@@ -0,0 +1,12 @@
+package com.internsavy.calculator;
+
+
+public interface Operation {
+	public int sum(int x, int y);
+	public int substraction(int x, int y);
+	public int multiplication(int x, int y);
+	public int division(int x, int y);
+}
+
+
+
